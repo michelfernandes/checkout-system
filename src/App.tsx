98ebc863d07a77372ae82c62ea4ee08b67423056
Michel Fernandes <michelmf_rs@hotmail.com>
2021-12-11 18:09:17 -0300

    feat: add promotional rules table

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ProductTable } from './components/ProductTable'
+import { PromotionalRulesTable } from './components/PromotionalRulesTable'
 
 function App() {
+
+const promotionRules = [
+  {
+    active: true,
+    rule: "If you buy 3 pizza, one is free"
+  },
+  {
+    active: false,
+    rule: "If you spend over 30EUR you get 10% off the total price"
+  }
+]
+
   return (
+      <div>
         <ProductTable />
-        
+        <div>
+          <PromotionalRulesTable rowData={promotionRules}/>
+        </div>
+      </div>
   );
 }
 
---
+++ src/components/PromotionalRulesTable.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+type PromotionRule = {
+    active: boolean,
+    rule: string
+}
+
+export interface PromotionalRulesTableProps {
+  rowData: PromotionRule[];
+}
+export const PromotionalRulesTable: React.FC<PromotionalRulesTableProps> = ({
+    rowData,
+}) => {
+
+  return (
+    <table>
+    <tr>
+      <th>Active</th>
+      <th>Promotional Rule</th>
+    </tr>
+    {rowData.map((data) => (
+      <tr>
+        <td><input type="checkbox" checked={data.active}/></td>
+        <td>{data.rule}</td>
+      </tr>
+    ))}
+    
+  </table>
+  );
+};
